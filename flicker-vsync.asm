2d42db5e9279e0fcd38202db4766de65b7fe6147
Christian Stigen Larsen <csl@csl.name>
2016-04-10 21:03:36 +0200

    Flicker examples

---
+++ flicker-vsync.asm
@@ -0,0 +1,45 @@
+!cpu 6502
+!source "basic-loader.asm"
+
+* = $c000
+sei
+ldx #$00
+
+ldy #$7f
+sty $dc0d
+sty $dd0d
+lda $dc0d
+lda $dd0d
+
+lda #$01
+sta $d01a
+
+lda #<.flicker
+ldx #>.flicker
+sta $314
+stx $315
+
+; Line number, would be better ot do this on retrace
+lda #$00
+sta $d012
+
+lda $d011
+and #$7f
+sta $d011
+
+cli
+jmp *
+
+.flicker
+  dec $d019
+
+  ldx .color
+  stx $d021
+  stx $d020
+  inx
+  stx .color
+
+  jmp $ea81
+
+.color
+  !byte $00
--- flicker.asm
+++ flicker.asm
@@ -1,6 +1,7 @@
 !cpu 6502
 !source "basic-loader.asm"
 
+* = $c000
 ldx #$00
 .loop
 stx $d021
