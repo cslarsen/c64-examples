2d34d1df65231dfb32bebced11e49cce6b05236e
Christian Stigen Larsen <csl@csl.name>
2016-04-16 12:30:48 +0200

    Adds some comments

--- flicker.asm
+++ flicker.asm
@@ -1,7 +1,20 @@
 !source "basic-loader.asm"
 
+; Some constant memory addresses
+background = $d020
+border = $d021
+
+; Start address of program
+; (which the basic loader will jump to)
 * = $c000
+
+; Set background and border to black
+ldx #$00
+stx background
+stx border
+
+; Flicker border and backround
 .loop
-inc $d020
-inc $d021
-jmp .loop
+  inc background
+  inc border
+  jmp .loop
